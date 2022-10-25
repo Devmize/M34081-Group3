d264e1c30e480bfe9cee2a4d0301ed21a1c5a3de
Devmize <devmize@gmail.com>
2022-10-25 11:10:22 +0300

    load order entity

--- src/main/kotlin/com/itmo/microservices/demo/order/Order.kt
+++ src/main/kotlin/com/itmo/microservices/demo/order/Order.kt
@@ -1,4 +1,27 @@
 package com.itmo.microservices.demo.order
 
+import com.itmo.microservices.demo.product.Product
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
 class Order {
+
+    @Id
+    @GeneratedValue
+    var id: UUID? = null
+    var products: Array<Product>? = null
+    var price: Double? = null
+    var date: Date? = null
+
+    constructor()
+
+    constructor(id: UUID?, products: Array<Product>?, price: Double?, date: Date?) {
+        this.id = id
+        this.products = products
+        this.price = price
+        this.date = date
+    }
 }
