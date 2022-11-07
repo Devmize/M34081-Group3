5163aa29d8e538a6ae564807b238c812b82197c0
Nikita <gladiolusovuch@gmail.com>
2022-11-07 20:22:12 +0300

    add deliv with api, config, logic, projections

---
+++ src/main/kotlin/com/itmo/microservices/demo/deliv/logic/DeliveryAggregateState.kt
@@ -0,0 +1,18 @@
+package com.itmo.microservices.demo.deliv.logic
+
+import com.itmo.microservices.demo.delivery.api.DeliveryAggregate
+import ru.quipy.domain.AggregateState
+import java.math.BigDecimal
+import java.sql.Timestamp
+import java.util.UUID
+
+class DeliveryAggregateState: AggregateState<UUID, DeliveryAggregate> {
+    private lateinit var DeliveryId: UUID
+    private var TimeSlot: Timestamp = Timestamp(0,0,0,0,0);
+    private var address: String? = ""
+    private var phoneNumber: BigDecimal? = BigDecimal.ZERO
+
+    override fun getId(): UUID {
+        return this.DeliveryId
+    }
+}
