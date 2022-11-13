32cbfa6a9fada17565ab375a9db05165309aa1a9
unknown <egor111orexov@gmail.com>
2022-11-14 00:47:03 +0300

    remake

--- src/main/kotlin/com/itmo/microservices/demo/payment/api/PaymentEvents.kt
+++ src/main/kotlin/com/itmo/microservices/demo/payment/api/PaymentEvents.kt
@@ -6,11 +6,9 @@ import java.math.BigDecimal
 import java.util.*
 
 const val PAYMENT_ATTEMPT = "PAYMENT_ATTEMPT_EVENT"
-//const val PAYMENT_COMPLETED_SUCCESSFULLY = "PAYMENT_COMPLETED_SUCCESSFULLY_EVENT"
-//const val PAYMENT_FAILED = "PAYMENT_FAILED_EVENT"
 
 enum class PaymentStatus{
-    Pending, Success, Failure, Initialized
+    Pending, Success, Failure
 }
 
 @DomainEvent(name = PAYMENT_ATTEMPT)
@@ -23,25 +21,3 @@ class PaymentAttemptEvent(
     name = PAYMENT_ATTEMPT,
     createdAt = System.currentTimeMillis(),
 )
-
-/*
-@DomainEvent(name = PAYMENT_COMPLETED_SUCCESSFULLY)
-class PaymentCompletedSuccessfullyEvent(
-    val paymentId: UUID,
-    val orderId: UUID?,
-    val sum: BigDecimal?,
-) : Event<PaymentAggregate>(
-    name = PAYMENT_COMPLETED_SUCCESSFULLY,
-    createdAt = System.currentTimeMillis(),
-)
-
-@DomainEvent(name = PAYMENT_FAILED)
-class PaymentFailedEvent(
-    val paymentId: UUID,
-    val orderId: UUID?,
-    val sum: BigDecimal?,
-) : Event<PaymentAggregate>(
-    name = PAYMENT_FAILED,
-    createdAt = System.currentTimeMillis(),
-)
-*/
