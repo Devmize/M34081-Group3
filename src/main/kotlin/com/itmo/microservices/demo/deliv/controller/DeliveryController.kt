675ce3c4aac4a503b9d17a597647e4f34476aa65
Nikita <gladiolusovuch@gmail.com>
2023-02-20 22:59:36 +0300

    add controls

--- src/main/kotlin/com/itmo/microservices/demo/deliv/controller/DeliveryController.kt
+++ src/main/kotlin/com/itmo/microservices/demo/deliv/controller/DeliveryController.kt
@@ -1,27 +1,32 @@
-package com.itmo.microservices.demo.deliv.api
+package com.itmo.microservices.demo.deliv.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
-import ru.quipy.core.annotations.AggregateType
-import ru.quipy.domain.Aggregate
+import com.itmo.microservices.demo.deliv.api.DeliveryAggregate
+import com.itmo.microservices.demo.deliv.logic.DeliveryAggregateState
+import ru.quipy.core.EventSourcingService
 import java.util.*
-
 @RestController
 @RequestMapping("/Delivery")
 class DeliveryController(
     val deliveryEsService: EventSourcingService<UUID, DeliveryAggregate, Account>
 ) {
 
-    @PostMapping("/{holderId}")
-    fun createAccount(@PathVariable holderId: UUID) : DeluveryCreatedEvent {
-        return accountEsService.create { it.createNewDelivery(holderId = holderId) }
+    @PostMapping("/{UUID/delivery}")
+    fun attemptDelivery(@PathVariable holderId: UUID) : DeliveryAttemptEvent {
+        return deliveryEsService.create { it.createNewDelivery(holderId = holderId) }
     }
 
-    @GetMapping("/{deluveryId}")
-    fun getDelivery(@PathVariable accountId: UUID) : Deluvery? {
-        return DeluveryEsService.getState(deliveryId)
+    @PostMapping("/{UUID/delivery}")
+    fun CompletedSuccessfullyDelivery(@PathVariable holderId: UUID) : DeliveryCompletedSuccessfullyEvent {
+        return deliveryEsService.getState(holderId , "Success");
+    }
+
+    @PostMapping("/{UUID/delivery}")
+    fun FailedDelivery(@PathVariable holderId: UUID) : DeliveryFailedEvent {
+        return deliveryEsService.getState(holderId, "Success");
     }
 }
