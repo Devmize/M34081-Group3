a2e359e4848a4fbb8ca2e910973981219090701d
ssevenseass <k-kuksenko@mail.com>
2023-03-28 10:48:21 +0300

    delete comments

--- src/main/kotlin/com/itmo/microservices/demo/catalog/controller/CatalogController.kt
+++ src/main/kotlin/com/itmo/microservices/demo/catalog/controller/CatalogController.kt
@@ -43,10 +43,6 @@ class CatalogController(val catalogEsService: EventSourcingService<String, Catal
         @RequestParam size: Int,
         @PathVariable catalogId: String
     ): List<CatalogItemDto>? {
-        // спросить почему не передается catalogId, спроектировано так, что каталог - агрегат
-        // каталог оформлен как агрегат. чтобы получить конкретный агрегат нам нужен id, пусть агрегат всего один (singleton), однако у нас нет никакой логики по его созданию
-        // поэтому мы его никак не сможем получить
-        // тогда нам неоткуда брать этот самый список элементов
         val catalog = catalogEsService.getState(catalogId)
         if (catalog != null) {
             return catalog
